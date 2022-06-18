11cf1c2b6afc60161b6545f48e46a25cc501f1fa
nvlannasik <annasikn@yahoo.com>
2022-06-18 22:30:44 +0700

    create register page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { RegisterBg } from '../../assets';
+import { Button, Gap, Input, Link } from '../../components';
+import './register.scss';
 
 const Register = () => {
   return (
-    <div>
-      <p>Register</p>
+    <div className="main-page">
+      <div className="left">
+        <img src={RegisterBg} className="bg-images" alt="RegisterBg" />
+      </div>
+      <div className="right">
+        <p>Register</p>
+        <Input label="Full Name" placeholder="Full Name" />
+        <Gap height={18} />
+        <Input label="Email" placeholder="Email" />
+        <Gap height={18} />
+        <Input label="Password" placeholder="Password" />
+        <Gap height={50} />
+        <Button title="Register" />
+        <Gap height={50} />
+        <Link title="Kembali ke Login" />
+      </div>
     </div>
   );
 };
