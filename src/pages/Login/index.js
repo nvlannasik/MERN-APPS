ddd4cc69fdfe154017642bc55bfcf92c9dc26847
nvlannasik <annasikn@yahoo.com>
2022-06-19 00:12:22 +0700

    create login page & fixing styling

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { LoginBg } from '../../assets';
+import { Button, Gap, Input, Link } from '../../components';
 
 const Login = () => {
   return (
-    <div>
-      <p>Login</p>
+    <div className="main-page">
+      <div className="left">
+        <img src={LoginBg} className="bg-images" alt="RegisterBg" />
+      </div>
+      <div className="right">
+        <p>Login</p>
+        <Input label="Email" placeholder="Email" />
+        <Gap height={18} />
+        <Input label="Password" placeholder="Password" />
+        <Gap height={50} />
+        <Button title="Login" />
+        <Gap height={50} />
+        <Link title="Belum punya akun? Daftar Disini" />
+      </div>
     </div>
   );
 };
