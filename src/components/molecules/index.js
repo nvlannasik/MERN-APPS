4c917200a322dfbaf6dc5c766fb79486426631a7
nvlannasik <annasikn@yahoo.com>
2022-06-19 23:17:04 +0700

    create blog post

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -2,4 +2,5 @@ import Header from './Header';
 import Footer from './Footer';
 import BlogItem from './BlogItem';
 
+
 export { Header, Footer, BlogItem };
--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Button, Input, TextArea, Upload, Gap } from '../../components';
+import './createBlog.scss';
 
 const CreateBlog = () => {
-  return <div>CreateBlog</div>;
+  return (
+    <div className="blog-post">
+      <p className="title">Create Blog</p>
+      <Input label="Post Title" />
+      <p>Upload image</p>
+      <Upload />
+      <TextArea />
+      <Gap height={20} />
+      <div className="button-action">
+        <Button title="Save" />
+      </div>
+    </div>
+  );
 };
 
 export default CreateBlog;
