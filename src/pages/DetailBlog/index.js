9bb8b6b8df9a4fedfd37c6bffcbb485a9f8e3e5d
nvlannasik <annasikn@yahoo.com>
2022-06-19 23:55:40 +0700

    create detail blog

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { LoginBg } from '../../assets';
+import './detailBlog.scss';
 
 const DetailBlog = () => {
-  return <div>DetailBlog</div>;
+  return (
+    <div className="detail-blog-wrapper">
+      <img className="img-cover" src={LoginBg} alt="thumb" />
+      <p className="blog-title">Title</p>
+      <p className="blog-author">Author - date</p>
+      <p className="blog-body">
+        Veniam aliquip sunt nulla minim aliqua. Labore culpa qui eu irure dolor
+        exercitation. Culpa ea consectetur velit velit velit dolore ullamco quis
+        minim nulla. Dolor ullamco excepteur quis officia ullamco laborum ea
+        laboris id ex veniam laboris aliqua elit. Ut esse minim eiusmod magna
+        magna et amet mollit dolore.
+      </p>
+    </div>
+  );
 };
 
 export default DetailBlog;
